2c3093296d2c5719973c172b7f3c7e2c9abf0001
masoomeh12 <idarkms1288@gmail.com>
2026-05-24 15:21:51 +0330

    feat(dashboard): add User Profile component

---
+++ app/components/dashboard/UserProfile.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+
+export default function UserProfile({
+  email,
+  mobile,
+}: {
+  email?: string;
+  mobile?: boolean;
+}) {
+  const username = (email || "user@gmail.com").split("@")[0];
+
+  return (
+    <div className={mobile ? "lg:hidden mt-[3px]" : "flex items-center gap-3"}>
+      <div className="relative w-[52px] h-[52px] rounded-full overflow-hidden border-[2px] border-[#FF8DBB]">
+        <Image
+          src="/icons/avatar.png"
+          alt="avatar"
+          fill
+          className="object-cover"
+        />
+      </div>
+
+      {!mobile && (
+        <span className="text-[16px] font-medium text-[#303153] dark:text-white">
+          {username}
+        </span>
+      )}
+    </div>
+  );
+}
