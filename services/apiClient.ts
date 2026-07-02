9f6e2eb744adf1777458284dc4e577be42953250
movhde <movahedesalehivaziri@gmail.com>
2026-07-02 17:18:11 +0330

    fix: fix base url by using env variable

--- services/apiClient.ts
+++ services/apiClient.ts
@@ -1,14 +1,16 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 apiClient.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+  const token =
+    typeof window !== "undefined" ? localStorage.getItem("token") : null;
+
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
